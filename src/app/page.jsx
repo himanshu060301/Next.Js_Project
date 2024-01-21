3fb615a979d91208d007d14f5d2b6cc8d8eb2b89
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2024-01-21 21:51:15 +0530

    third commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import styles from './home.module.css';
+import Image from 'next/image';
+
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          It is a long established fact that a reader will be distracted 
+          by the readable content of a page when looking at its layout.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,13 @@
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Dev</div>
+      <div className={styles.text}>
+        Copyright @ All rights reserved.
+      </div>
+    </div>
   )
 }
 
